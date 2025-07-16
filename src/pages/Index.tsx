e45ae1e3894ae746e74b02cb7ce0b4e1697189cb
krishnarayalutv-pixel <krishnarayalutv@gmail.com>
2025-07-17 00:28:57 +0530

    Update Index.tsx
    updating order of images and hero section

--- src/pages/Index.tsx
+++ src/pages/Index.tsx
@@ -12,8 +12,8 @@ const Index = () => {
   return (
     <div className="min-h-screen bg-background">
       <Header />
-      <ServicesCarousel />
       <Hero />
+      <ServicesCarousel />
       <CounterMetrics />
       <AboutPreview />
       <WhatSetsUsApart />
